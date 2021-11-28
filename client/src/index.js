2fbc7871ce9f580b167d7176d785c5fa9e01d822
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-28 12:30:02 -0800

    starting refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Loader } from '@googlemaps/js-api-loader';
-import MapContainer from './Map.jsx';
 import './style.css';
 
 class App extends Component {
@@ -13,11 +11,8 @@ class App extends Component {
   render() {
     return(
       <div>
-        <h1>Random Location Generator + Tracker</h1>
-        <div id="description">Click anywhere on the map to jump to a new, randomly generated location in the world. If you like where you end up, click the marker pin to save the name and coordinates of that place for future reference!</div>
-        <br/>
-        <MapContainer />
-        <div id="map"></div>
+        <h1>RPG</h1>
+        <div id="description">Description here</div>
       </div>
     );
   }
