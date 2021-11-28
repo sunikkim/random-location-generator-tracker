70bbe339c3d589e0c101fbafbe805883cdc4c9f9
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-28 12:16:14 -0800

    prepping db for deploy

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
